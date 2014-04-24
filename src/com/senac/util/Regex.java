90602f3158fc33f98474c2c2698b0bbbab63fcd3
deyvid-fk <deyvid.fk@gmail.com>
2014-04-24 17:58:40 -0300

    Adicionei padrões de expressões regulares na class Regex;

--- src/com/senac/util/Regex.java
+++ src/com/senac/util/Regex.java
@@ -11,17 +11,9 @@ public enum Regex {
      */
     NOME_E_SOBRENOME("^[a-zA-ZÀ-Üà-ü]+( [a-zA-ZÀ-Üà-ü]+)+$"),
     /**
-     * * CPF: 223.185.357-44
-     */
-    CPF("^\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}$"),
-    /**
      * * RG
      */
     RG("^\\d{1,3}.\\d{3}.\\d{3}-[a-zA-Z0-9]$"),
-    /**
-     * * CNPJ
-     */
-    CNPJ("^\\d{3}.?\\d{3}.?\\d{3}/?\\d{3}-?\\d{2}$"),
     /**
      * * CEP
      */
@@ -38,7 +30,8 @@ public enum Regex {
     /**
      * * E-MAIL: EXEMPLO@EXEMPLO.COM
      */
-    EMAIL("^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"),
+    // EMAIL("^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"),
+    EMAIL("^[\\w!#\\$%&'\\*\\+\\-/=\\?\\^`\\{\\|\\}~]+(\\.[\\w!#\\$%&'\\*\\+\\-/=\\?\\^`\\{\\|\\}~]+)*@[a-zA-Z0-9]+([\\-]?[a-zA-Z0-9])+(\\.[a-zA-Z0-9]+([\\-]?[a-zA-Z0-9]){1,})+$"),
     /**
      * * Hora:Minuto
      */
@@ -62,8 +55,79 @@ public enum Regex {
     /**
      * * Corresponde a qualquer palavra ou frase na lista a seguir. adm admin
      */
-    LOGIN_ADM("(\\W|^)(adm|admin|)(\\W|$)");
+    LOGIN_ADM("(\\W|^)(adm|admin|)(\\W|$)"),
+    ALFA("^[a-zA-Z]+$"),
+    // / <summary>
+    // / Alfa e espaço
+    // / </summary>
+    ALFA_COM_ESPACO("^[a-zA-Z ]+$"),
+    // / <summary>
+    // / Alfanumérico
+    // / </summary>
+    ALFA_NUMERICO("^[a-zA-Z0-9]+$"),
+    // / <summary>
+    // / Alfanumérico e espaço
+    // / </summary>
+    ALFA_NUMERICO_COM_ESPACO("^[a-zA-Z0-9 ]+$"),
+    // / <summary>
+    // / Alfanumérico e separadores (espaço, tab, quebra de linha)
+    // / </summary>
+    ALFA_NUMERICO_COM_SEPARADORES("^[a-zA-Z0-9\\s]+$"),
+    // / <summary>
+    // / Alfa acentuado
+    // / </summary>
+    ALFA_ACENTUADO("^[a-zA-Z" + Regex.sAcc + "]+$"),
+    // / <summary>
+    // / Alfa e espaço acentuado
+    // / </summary>
+    ALFA_COM_ESPACO_ACENTUADO("^[a-zA-Z " + Regex.sAcc + "]+$"),
+    // / <summary>
+    // / Alfanumérico acentuado
+    // / </summary>
+    ALFA_NUMERICO_ACENTUADO("^[a-zA-Z0-9" + Regex.sAcc + "]+$"),
+    // / <summary>
+    // / Alfanumérico e espaço
+    // / a-z / A-Z / 0-9 / " " +acentuação
+    // / </summary>
+    ALFA_NUMERICO_COM_ESPACO_ACENTUADO("^[a-zA-Z0-9 " + Regex.sAcc + "]+$"),
+    // / <summary>
+    // / Alfanumérico e separadores (espaço, tab, quebra de linha)
+    // / </summary>
+    ALFA_NUMERICO_COM_SEPARADORES_ACENTUADO("^[a-zA-Z0-9\\s" + Regex.sAcc + "]+$"),
+    // / <summary>
+    // / Word (alfanuméricos não acentuados, números e underscore)
+    // / </summary>
+
+    ALFA_NUMERICO_SUBLINHADO("^[\\w]+$"),
+    // / <summary>
+    // / Aceita caracteres, número, sublinhado e ponto
+    // / </summary>
+    ALFA_NUMERICO_SUBLINHADO_COM_PONTO("^[\\w.]+$"),
+    // / <summary>
+    // / Ids
+    // / </summary>
+    ID("^{*[\\w|-]+}*$"),
+    // / <summary>
+    // / CPF
+    // / 999.999.999-99 ou 99999999999
+    // / </summary>
+    CPF("^((\\d{3}.\\d{3}.\\d{3}-\\d{2})|(\\d{3}\\d{3}\\d{3}\\d{2}))$"),
+    // / <summary>
+    // / CNPJ
+    // / 99.999.999/9999-99 ou 99999999999999
+    // / </summary>
+    CNPJ("^((\\d{2}.\\d{3}.\\d{3}\\/\\d{4}-\\d{2})|(\\d{2}\\d{3}\\d{3}\\d{4}\\d{2}))$"),
+    // / <summary>
+    // / Ip
+    // / </summary>
+    IP("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$"),
+    // / <summary>
+    // / Ip Faixa
+    // / </summary>
+    IP_FAIXA("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.(\\d{1,3}|\\*)$");
+
     public String expressao;
+    public final static String sAcc = "|ç|ã|ñ|õ|á|é|í|ó|ú|à|è|ì|ò|ù|ä|ë|ï|ö|ü|â|ê|î|ô|û|Ç|Ã|Ñ|Õ|Á|É|Í|Ó|Ú|À|È|Ì|Ò|Ù|Ä|Ë|Ï|Ö|Ü|Â|Ê|Î|Ô|Û|";
 
     public String getExpressao() {
 
